79afc9be537fdee427c2e92466c4ebf8a814d592
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-26 14:43:31 +0530

    Update servicebus_failover_to_secondary.ps1

--- servicebus_failover_to_secondary.ps1
+++ servicebus_failover_to_secondary.ps1
@@ -1,9 +1,9 @@
-﻿param(
-[Parameter(Mandatory=$true)][string]$resourcegroupname,            #Enter the name of resource group where the secondary namespace is existing
-[Parameter(Mandatory=$true)][string]$aliasname,                    #Enter the alias name     
-[Parameter(Mandatory=$true)][string]$secondary_namespace,          #Enter the secondary namespace
-[Parameter(Mandatory=$true)]$subscriptionId_of_secondary_namespace #Enter the subscriptionID of secondary namespace
+param(
+[Parameter(Mandatory=$true)][string]$secondary_servicebus_resource_group_name,            #Enter the name of resource group where the secondary namespace is existing
+[Parameter(Mandatory=$true)][string]$servicebus_aliasname,                                #Enter the alias name     
+[Parameter(Mandatory=$true)][string]$secondary_servicebus_namespace_name,                 #Enter the secondary namespace
+[Parameter(Mandatory=$true)]$subscriptionId_of_secondary_namespace                        #Enter the subscriptionID of secondary namespace
 )
 
 #Below command invokes failover from Primary to secondary namespace by pointing alias to secondary namespace.
-Set-AzServiceBusGeoDRConfigurationFailOver -Name $aliasname -NamespaceName $secondary_namespace -ResourceGroupName $resourcegroupname -SubscriptionId $subscriptionId_of_secondary_namespace
+Set-AzServiceBusGeoDRConfigurationFailOver -Name $servicebus_aliasname -NamespaceName $secondary_servicebus_namespace_name -ResourceGroupName $secondary_servicebus_resource_group_name -SubscriptionId $subscriptionId_of_secondary_namespace
