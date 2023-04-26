750186e7e62244b9cbf30a12602cd5fd1643c7e1
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-26 13:05:51 +0530

    Add files via upload

---
+++ Invoke_Service_Bus_Failover_to_Secondary_param.ps1
@@ -0,0 +1,9 @@
+﻿param(
+[Parameter(Mandatory=$true)][string]$resourcegroupname,            #Enter the name of resource group where the secondary namespace is existing
+[Parameter(Mandatory=$true)][string]$aliasname,                    #Enter the alias name     
+[Parameter(Mandatory=$true)][string]$secondary_namespace,          #Enter the secondary namespace
+[Parameter(Mandatory=$true)]$subscriptionId_of_secondary_namespace #Enter the subscriptionID of secondary namespace
+)
+
+#Below command invokes failover from Primary to secondary namespace by pointing alias to secondary namespace.
+Set-AzServiceBusGeoDRConfigurationFailOver -Name $aliasname -NamespaceName $secondary_namespace -ResourceGroupName $resourcegroupname -SubscriptionId $subscriptionId_of_secondary_namespace
